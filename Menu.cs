78aecd72f1a557318954b01faf41e39ca9dc951d
kanema06 <karennegrin853@gmail.com>
2025-06-15 22:06:47 -0400

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,44 @@
+using codes;
+using Godot;
+using Grid;
+using System;
+using System.IO;
+
+public partial class Menu : Control
+{
+	public static bool abrirarchivo=false;
+	private Panel panel;
+	private FileDialog fileDialogmenu;
+
+	public void _on_acerca_de_pressed()
+	{
+		panel.Visible=true;
+	}
+	public void _on_volver_pressed()
+	{
+		panel.Visible=false;
+	}
+	public void _on_abrir_archivo_pressed()
+	{
+		abrirarchivo=true;
+		GetTree().ChangeSceneToFile("res://Scenes/CompilerUi.tscn");
+	}
+	
+	public void _on_salir_pressed()
+	{
+	     GetTree().Quit();
+	}
+	public override void _Ready()
+	{
+
+		panel=GetNode<Panel>("PanelAcercaDe");
+	}
+	public void _on_nuevo_documento_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/CompilerUi.tscn");
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
